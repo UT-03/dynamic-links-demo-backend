2055536d1603d11d32cd3982344b18e9db12e747
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-19 09:34:28 +0530

    feat: add user model

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    password: { type: String, default: "" },
+    invitee: {
+        userId: { type: mongoose.Types.ObjectId, ref: 'User1', default: null },
+        isRewarded: { type: Boolean, default: false }
+    },
+    invites: [{
+        userId: { type: mongoose.Types.ObjectId, ref: 'User1' },
+        isConfirmed: { type: Boolean, default: false }
+    }]
+});
+
+module.exports = mongoose.model('User1', userSchema);
