d35e1535e1600e741abe62a5202fbdcdb45884a8
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-20 08:52:03 +0530

    bug: make password a required field in user model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    password: { type: String, default: "" },
+    password: { type: String, required: true },
     invitee: {
         userId: { type: mongoose.Types.ObjectId, ref: 'User1', default: null },
         isRewarded: { type: Boolean, default: false }
